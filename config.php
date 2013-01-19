d86303a8accdf7c20589bfbfde2c8e6fce4c13b4
erkan <erkan.valentin@unistra.fr>
2013-01-19 16:58:57 +0100

    test

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
 define("PIWIGO", "/home/erkan/public_html/piwigo");
-define("OWNCLOUD", "/home/erkan/public_html/owncloud");
-define("PHPSYSINFO", "/home/erkan/public_html/phpsysinfo");
-define("MEDIAWIKI", "/home/erkan/public_html/mediawiki");
-define("DOKUWIKI", "/home/erkan/public_html/dokuwiki");
-define("PHPMYADMIN", "/home/erkan/public_html/phpmyadmin");
-define("WORDPRESS", "/home/erkan/public_html/wordpress");
-define("DOTCLEAR", "/home/erkan/public_html/dotclear");
+define("OWNCLOUD", "/var/www/owncloud");
+define("PHPSYSINFO", "/home/erkan/public_html/phpsi");
+#define("MEDIAWIKI", "/home/erkan/public_html/mediawiki");
+#define("DOKUWIKI", "/home/erkan/public_html/dokuwiki");
+define("PHPMYADMIN", "/usr/share/phpmyadmin");
+#define("WORDPRESS", "/home/erkan/public_html/wordpress");
+#define("DOTCLEAR", "/home/erkan/public_html/dotclear");
 
 ?>
