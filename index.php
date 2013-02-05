c42fe3ed3c8f48d256b6a2dd60bccbd67bbd1207
develle <contact@develle.fr>
2013-02-05 23:50:22 +0100

    separate js and replace config.php by config.json

--- index.php
+++ index.php
@@ -67,63 +67,7 @@
     </div> <!-- /container -->
 
     <script src="js/jquery.js"></script>
-
-    <script type="text/javascript">
-        
-    $(document).ready(function(){
-    
-    });
-        
-    $(document).ajaxStart(function(){
-        $("#loader").show();
-    });
-    $(document).ajaxStop(function(){
-        $("#loader").hide();
-    });     
-    
-    
-    <?php 
-    include('config.php');
-    foreach ($servers as $key => $value) {
-    ?>
-    
-    $.ajax({
-        type: "GET",
-        cache: false,
-        dataType: "text",
-        url: "call_probe.php?server=<?php echo $value ?>",
-        success: function (data) {
-        
-            json_version = JSON.parse(data);
-
-            $.each(json_version, function(key, value){
-                $.each(json_version[key], function(app, version){
-                    if(version["local"] == version["remote"]) {
-                        var label = "label-success";
-                    }
-                    else if(version["local"] == 0 || version["remote"] == 0) {
-                        var label = "";
-                    }
-                    else {
-                        var label = "label-warning";
-                    }
-                   
-                    $("#tbl_content").append("<tr>"+
-                    "<td>"+app+"</td>"+
-                    "<td><span class=\"label "+label+"\">"+version["local"]+"</span></td>"+
-                    "<td>"+version["remote"]+"</td>/tr>");
-                });
-            });
-            
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrows) {
-            console.log("error");
-        }
-    });
-
-    <?php } ?>
-
-    </script>
+    <script src="js/display.js"></script>
 
   </body>
 </html>
