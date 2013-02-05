9ed870eddadda3958fb8fb6c9f984b67c570a292
Erkan Valentin <valentin@clarinet.u-strasbg.fr>
2013-02-05 22:54:09 +0100

    add all remote tests

--- tests/RemoteTest.php
+++ tests/RemoteTest.php
@@ -15,5 +15,12 @@ class remoteTest extends PHPUnit_Framework_TestCase {
         $this->assertRegExp("/(.*)\.(.*)\.(.*)/",check_owncloud_remote());
         $this->assertRegExp("/(.*)\.(.*)\.(.*)/",check_phpsysinfo_remote());
         $this->assertRegExp("/(.*)\.(.*)\.(.*)/",check_mediawiki_remote());
+        //$this->assertRegExp("/(.*)\.(.*)\.(.*)/",check_dokuwiki_local());
+        $this->assertRegExp("/(.*)\.(.*)\.(.*)/",check_phpmyadmin_remote());
+        $this->assertRegExp("/(.*)\.(.*)\.(.*)/",check_wordpress_remote());
+        $this->assertRegExp("/(.*)\.(.*)\.(.*)/",check_dotclear_remote());
+        $this->assertRegExp("/(.*)\.(.*)\.(.*)/",check_gitlab_remote());
+        $this->assertRegExp("/(.*)\.(.*)\.(.*)/",check_symfony_remote());
+        $this->assertRegExp("/(.*)\.(.*)\.(.*)/",check_checker_remote());
     }
 }
