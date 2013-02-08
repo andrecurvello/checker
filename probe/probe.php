a95b1dfeb0eddcb354b899fcc1e8737f159f332e
rk4an <erkan2005@gmail.com>
2013-02-08 21:55:03 +0100

    cleanup last commit

--- probe/probe.php
+++ probe/probe.php
@@ -7,11 +7,8 @@ $json_version = array();
 
 foreach ($hosts as $name=>$url) {
     $json_version[$name]['local'] = call_user_func("check_".strtolower($name)."_local",$url);
-    $json_version[$name]['remote'] = call_user_func("check_".strtolower($name)."_remote",$url);
+    $json_version[$name]['remote'] = call_user_func("check_".strtolower($name)."_remote");
 }
-
-$json_version['Checker']['local'] = check_checker_local();
-$json_version['Checker']['remote'] = check_checker_remote();
 
 echo json_encode(array($json_version));
 
