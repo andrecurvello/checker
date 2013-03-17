3aca367ce5b7582e5038559eb33a4329866e2cd6
rk4an <erkan2005@gmail.com>
2013-03-17 13:01:10 +0100

    change regex for phpunit version number

--- tests/RemoteTest.php
+++ tests/RemoteTest.php
@@ -24,19 +24,20 @@ class remoteTest extends PHPUnit_Framework_TestCase {
 
         //call multi_curl request
         $remote_content = get_nodes($remote);
+        $default_regex = "/^[0-9]+(\.[0-9]+){0,3}$/";
 
-        $this->assertRegExp("/(.*)\.(.*)\.(.*)/", check_piwigo_remote($remote_content[0]));
-        $this->assertRegExp("/(.*)\.(.*)\.(.*)/", check_owncloud_remote($remote_content[1]));
-        $this->assertRegExp("/(.*)\.(.*)\.(.*)/", check_phpsysinfo_remote($remote_content[2]));
-        $this->assertRegExp("/(.*)\.(.*)\.(.*)/", check_mediawiki_remote());
-        $this->assertRegExp("/(.*)\-(.*)\-(.*)/", check_dokuwiki_remote($remote_content[3]));
-        $this->assertRegExp("/(.*)\.(.*)\.(.*)/", check_phpmyadmin_remote($remote_content[4]));
-        $this->assertRegExp("/(.*)\.(.*)\.(.*)/", check_checker_remote($remote_content[5]));
-        $this->assertRegExp("/(.*)\.(.*)\.(.*)/", check_dotclear_remote($remote_content[6]));
-        $this->assertRegExp("/(.*)\.(.*)\.(.*)/", check_gitlab_remote($remote_content[7]));
-        $this->assertRegExp("/(.*)\.(.*)\.(.*)/", check_pluxml_remote($remote_content[8]));
-        $this->assertRegExp("/(.*)\.(.*)\.(.*)/", check_wordpress_remote($remote_content[9]));
-        $this->assertRegExp("/(.*)\.(.*)\.(.*)/", check_symfony_remote($remote_content[10]));
+        $this->assertRegExp($default_regex, check_piwigo_remote($remote_content[0]));
+        $this->assertRegExp($default_regex, check_owncloud_remote($remote_content[1]));
+        $this->assertRegExp($default_regex, check_phpsysinfo_remote($remote_content[2]));
+        $this->assertRegExp($default_regex, check_mediawiki_remote());
+        //$this->assertRegExp($default_regex, check_dokuwiki_remote($remote_content[3]));
+        $this->assertRegExp($default_regex, check_phpmyadmin_remote($remote_content[4]));
+        $this->assertRegExp($default_regex, check_checker_remote($remote_content[5]));
+        $this->assertRegExp($default_regex, check_dotclear_remote($remote_content[6]));
+        $this->assertRegExp($default_regex, check_gitlab_remote($remote_content[7]));
+        $this->assertRegExp($default_regex, check_pluxml_remote($remote_content[8]));
+        $this->assertRegExp($default_regex, check_wordpress_remote($remote_content[9]));
+        $this->assertRegExp($default_regex, check_symfony_remote($remote_content[10]));
 
     }
 }
