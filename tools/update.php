302aeba612c90e2d5e39e2c63cbb4f134c312692
erkan <erkan.valentin@unistra.fr>
2013-03-29 15:49:29 +0100

    fix

--- tools/update.php
+++ tools/update.php
@@ -3,27 +3,26 @@
 set_time_limit(0);
 
 //settings
-$frequency = 1; //minutes
+$frequency = 60; //minutes
 $page = "http://localhost/~rk4an/checker/probe/probe.php";
-
-//notifications by email
 $email = "***";
-$title = "New update detect by checker";
 
+$title = "Checker: new update for ";
 $apps = array();
 
-while(1)
-{
+while(1) {
     //get the page
     $content = file_get_contents($page);
     $json = json_decode($content);
 
     //build array of remote version and check
     foreach ($json[0] as $app_name => $app_version) {
+
         echo  $app_name . " : " . $app_version->{'remote'} . "\n";
+        
         if(array_key_exists($app_name,$apps)) {
             if($apps[$app_name] =! $app_version->{'remote'}) {
-                mail($email,$title,$title);
+                mail($email, $title.$app_name, $title.$app_name);
                 $apps[$app_name] = $app_version->{'remote'};
             }
         }
